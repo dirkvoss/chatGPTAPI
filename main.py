f6bdd45e775253248e4f280c01fd0b28b0e8bcb6
dirkvoss <mail@dirk-voss.com>
2023-08-09 15:46:46 +0200

    included gtts 1st version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 import openai
+from gtts import gTTS
+from playsound import playsound
+
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,7 +19,12 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 # read prompt from keyboard as string
 prompt = input("Frage eingeben: ")
-
 response = get_completion(prompt)
+
+language = 'de'
+myobj = gTTS(text=response, lang=language, slow=False)
+myobj.save("response.mp3")
+
+playsound("/Users/dvoss/Projects/phyton/chatGPT/response.mp3")
 
 print(response)
