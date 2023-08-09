4de1954d0b1b17c6222617ab1fc033953e80f492
dirkvoss <mail@dirk-voss.com>
2023-08-09 14:24:28 +0200

    erster connect mit response

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("OPENAI_API_KEY")
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0
+        )
+    return response.choices[0].message["content"]
 
-print (api_key)
+prompt = "ich brauche ein iobroker javascript das konitnierlich die Variable alias.0.Strom.aktSpeicherBeladung_Watt überwacht. Wenn diese negativ wird  soll die Variable awtrix-light.0.apps.speicherentladen.visible  auf True  und awtrix-light.0.apps.speicherladen.visible auf false gesetzt werden. Wenn sie aber positiv  oder 0 wird soll die Variable awtrix-light.0.apps.speicherentladen.visible  auf False und awtrix-light.0.apps.speicherladen.visible auf True gesetzt werden."
+
+response = get_completion(prompt)
+
+print(response)
